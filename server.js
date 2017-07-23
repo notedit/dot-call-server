969c72132ad51a26abea83fd8b583942b6ede2c4
notedit <notedit@gmail.com>
2017-07-24 01:46:27 +0800

    add more code

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const sdpTransform = require('sdp-transform');
 const EventEmitter = require('events').EventEmitter;
 const expressWs = require('express-ws');
 
+const logger = require('./logger')();
 
 const app = express();
 const webServer = http.createServer(app);
@@ -21,6 +22,41 @@ class Peer extends EventEmitter {
 	constructor(server, socket){
 		super()
 		this.setMaxListeners(Infinity)
+
+        this._server = server
+        this._socket = socket
+        this._id = socket.id 
+
+        this._closed = false;
+
+
+        socket.on('message', async (msg) =>{
+			try {
+				await this.onMessage(msg);
+			} catch (error) {
+				logger.error('onMessage : ', error);
+			}
+		});
+
+		socket.on('close',() => {
+			
+
+			self._socket = null;
+			this.close();
+		});
+
+    }
+    get id(){
+        return this._id
+    }
+    async onMessage(message){
+        
+		let self = this;
+		logger.debug('message ', message);
+
+    }
+    close(){
+        
     }
 
 }
@@ -31,13 +67,26 @@ class Server extends EventEmitter {
     constructor(){
         super()
         this.setMaxListeners(Infinity)
+
+        this.peers  = new Map(); 
     }
 }
 
 
+const server = new Server();
+
 
 app.ws('/ws', async (socket, req) => {
 
+	socket.id =  udid('s' + (new Date()).getTime())
+    let peer = new Peer(server,socket);
+
+    server.peers.set(peer.id,peer);
+
+	peer.on('close',() =>{
+		logger.debug('server  peer close ', peer.id);
+		server.peers.delete(peer.id);
+	});
 
 });
 
